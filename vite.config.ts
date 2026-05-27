cdada6449769120ecba64c6698ffa9fba0e6253c
GuoZY <a310037397@qq.com>
2026-05-27 14:58:27 +0800

    chore: vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/CI-CD-Test/',  // 改成你的仓库名，注意前后都有斜杠
+  // 如果是部署在根域名（如 username.github.io）则用 base: '/'
 })
